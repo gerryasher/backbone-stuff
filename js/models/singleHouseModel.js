efee14a3c99d9e3543e6b0ce09f06740d2cb6e25
gerryasher <gerryasher@gmail.com>
2017-01-19 17:13:15 -0800

    Update singleHouseModel.js

--- js/models/singleHouseModel.js
+++ js/models/singleHouseModel.js
@@ -1,22 +1,5 @@
 var app = app || {};
 
 app.SingleHouse = Backbone.Model.extend({
-    //idAttribute: "id",
-    
-    tagName: 'li',
-    className: 'list-view-item skill',
-
-    template: _.template($("#singleHouse").html()),
-    render: function() {
-        var houseTemplate = this.template(this.model.toJSON());
-        this.$el.html(houseTemplate);
-        return this;
-    },
-    
-    events: {
-        'click .skill': 'skillClick'
-    },
-    skillClick: function(event) {
-        console.log('clicked: ', event);
-    }
+    idAttribute: "id"
 });
