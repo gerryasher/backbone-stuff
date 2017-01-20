1ebc9242af62f157d9cefb38f01b7da5d0b35970
gerryasher <gerryasher@gmail.com>
2017-01-19 17:13:32 -0800

    Update multiHousesView.js

--- js/views/multiHousesView.js
+++ js/views/multiHousesView.js
@@ -1,11 +1,19 @@
 var app = app || {};
 
 app.MultiHousesView = Backbone.View.extend({
+    
+    tagName: 'ul',
+    className: 'list-view',
+
+    template: _.template($("#multiHouses").html()),
     render: function() {
-        var self = this;
-        this.model.each(function(house) {
-            var houseView = new app.SingleHouseView({ model: house });
-            self.$el.append(houseView.render().$el);
-        });
+        //var self = this;
+        this.collection.each(this.addHouse, this);
+        return this;
+    },
+    addHouse: function(house){
+        var houseView = new app.SingleHouseView({ model: house });
+        this.$el.append(houseView.render().el);
     }
+
 });
